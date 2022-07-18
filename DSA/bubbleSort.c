94009f65984208541ba3654fac69a490db7078b9
achyut vardhan <achyutvardhan234@gmail.com>
2022-07-18 17:27:40 +0530

    insertion sort

--- DSA/bubbleSort.c
+++ DSA/bubbleSort.c
@@ -1,38 +1,37 @@
-  #include<stdio.h>
-  #include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void printArray(int* a,int n){
-    for (int  i = 0; i < n; i++)
+void printArray(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
-    
 }
 
-void bubbleSort(int *a,int n)
-{ 
-for (int i = 0; i < n-1; i++)
-{  
-    for (int j = 0; j < n-1-i; j++)
+void bubbleSort(int *a, int n)
+{
+    for (int i = 0; i < n - 1; i++)
     {
-        if (a[j]>a[j+1])
+        for (int j = 0; j < n - 1 - i; j++)
         {
-            int c = a[j];
-            a[j] = a[j+1];
-            a[j+1]= c;
+            if (a[j] > a[j + 1])
+            {
+                int c = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = c;
+            }
         }
-        
     }
-    
-}
-printf("\n");
+    printf("\n");
 }
 
-int sortedArraySwap(int *a, int n){
-        int count=0 ;
-    for (int i = 0; i < n-1; i++)
-    {  
-        if (a[i]>a[i+1])
+int sortedArraySwap(int *a, int n)
+{
+    int count = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] > a[i + 1])
         {
             count++;
             break;
@@ -40,22 +39,22 @@ int sortedArraySwap(int *a, int n){
     }
     return count;
 }
-  int main()
-  {
-    int a[] = {1,2 ,3,4,5,6 };
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5, 6};
     // int a[] = {1, 2, 5, 6, 12, 54, 625, 7, 23, 9, 987};
-    int n =6;
-    printArray(a,n); // array before sorting 
-    
-     if (sortedArraySwap(a,n)>0)
-     {
+    int n = 6;
+    printArray(a, n); // array before sorting
+
+    if (sortedArraySwap(a, n) > 0)
+    {
         printf("\narray is not sorted");
-        bubbleSort(a,n);
-        printArray(a,n);// array after sorting
-     }
-       else{
+        bubbleSort(a, n);
+        printArray(a, n); // array after sorting
+    }
+    else
+    {
         printf("array is already sorted");
-       }
+    }
     return 0;
-  }
-  
+}
